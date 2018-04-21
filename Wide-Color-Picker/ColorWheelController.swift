c8f993143473e53e10a109094ac207a65360eef2
mpospese <mpospese@me.com>
2018-04-21 19:16:29 +0200

    Implement color picker

---
+++ Wide-Color-Picker/ColorWheelController.swift
@@ -0,0 +1,169 @@
+/// Copyright (c) 2018 Razeware LLC
+/// 
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+/// 
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+/// 
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+/// 
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import Foundation
+import UIKit
+
+extension Gamut {
+  var imageName: String {
+    switch self {
+    case .displayP3: return "Color_Wheel"
+    case .sRGB: return "Color_Wheel_SRGB"
+    }
+  }
+}
+
+protocol ColorWheelControllerDelegate: class {
+  func didPick(color: UIColor)
+}
+
+class ColorWheelController: UIViewController {
+  
+  @IBOutlet weak var wheelImageView: UIImageView!
+  weak var delegate: ColorWheelControllerDelegate?
+  
+  let colorTargetView = UIImageView(image: #imageLiteral(resourceName: "Reticule"))
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.addSubview(colorTargetView)
+  }
+  
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    
+    moveTo(color: color)
+  }
+  
+  var gamut: Gamut = .displayP3 {
+    didSet {
+      let traits = UITraitCollection(displayGamut: gamut.displayGamut)
+      wheelImageView.image = UIImage(named: gamut.imageName, in: nil, compatibleWith: traits)
+      moveTo(colorTargetView.center)
+    }
+  }
+  
+  private(set) var color: UIColor = UIColor(named: "rwGreen")!
+}
+
+extension ColorWheelController {
+  
+  func moveTo(_ point: CGPoint) {
+    
+    let center = wheelImageView.center
+    let radius = min (wheelImageView.bounds.width / 2, wheelImageView.bounds.height / 2)
+    
+    // Calculate distance from touch to center of color wheel
+    let xOffset = point.x - center.x
+    let yOffset = point.y - center.y
+    let distance = sqrt(pow(xOffset, 2) + pow(yOffset, 2))
+    
+    var position = point
+    if distance > radius {
+      // If touch is outside of the color wheel,
+      // move it to the closest point along the circumference
+      position.x = center.x + (xOffset * (radius / distance));
+      position.y = center.y + (yOffset * (radius / distance));
+    }
+    
+    colorTargetView.center = position
+    
+    color = colorFrom(point: position)
+    delegate?.didPick(color: color)
+  }
+  
+  func colorFrom(point: CGPoint) -> UIColor {
+    
+    let center = wheelImageView.center
+    
+    let xOffset = point.x - center.x
+    let yOffset = point.y - center.y
+
+    // atan returns values between -pi and +pi
+    let atan = atan2(yOffset, xOffset)
+    // convert to a value between 0 and 2*pi
+    let angle = atan >= 0 ? atan : (atan + (2 * CGFloat.pi))
+    
+    let hue = 1 - (angle / (2 * CGFloat.pi))
+ 
+    let hsbColor = UIColor(hue: hue, saturation: 1, brightness: 1, alpha: 1)
+    
+    var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0
+    hsbColor.getRed(&red, green: &green, blue: &blue, alpha: nil)
+    
+    switch gamut {
+    case .displayP3: return UIColor(displayP3Red: red, green: green, blue: blue, alpha: 1)
+    case .sRGB: return UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
+  }
+  
+  func moveTo(color: UIColor) {
+    colorTargetView.center = positionFrom(color: color)
+  }
+  
+  func positionFrom(color: UIColor) -> CGPoint {
+    var hue: CGFloat = 0, saturation: CGFloat = 0, brightness: CGFloat = 0
+    color.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: nil)
+    
+    let angle = (1 - hue) * (2 * CGFloat.pi)
+    
+    let center = wheelImageView.center
+    let radius = min (wheelImageView.bounds.width / 2, wheelImageView.bounds.height / 2)
+    
+    let x = center.x + (cos(angle) * radius)
+    let y = center.y + (sin(angle) * radius)
+    
+    return CGPoint(x: x, y: y)
+  }
+}
+
+// MARK: Touch-based event handling
+extension ColorWheelController {
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    for t in touches {
+      moveTo(t.location(in: view))
+      break
+    }
+  }
+  
+  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    for t in touches {
+      moveTo(t.location(in: view))
+      break
+    }
+  }
+  
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+  }
+  
+  override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+  }
+  
+}
--- Wide-Color-Picker/ViewController.swift
+++ Wide-Color-Picker/ViewController.swift
@@ -29,32 +29,189 @@
 import Foundation
 import UIKit
 
-final class ViewController: UIViewController {
-
-    @IBOutlet weak var trackBackground: UIView!
-    @IBOutlet weak var gradientContainer: UIView!
-    @IBOutlet weak var slider: UISlider!
-    
+class ViewController: UIViewController {
+  
+  @IBOutlet weak var gamutSelector: UISegmentedControl!
+  @IBOutlet weak var pickerContainer: UIView!
+  @IBOutlet weak var trackBackground: UIView!
+  @IBOutlet weak var gradientContainer: UIView!
+  @IBOutlet weak var slider: UISlider!
+  @IBOutlet weak var swatchImageView: UIImageView!
+  @IBOutlet weak var p3RedLabel: UILabel!
+  @IBOutlet weak var rgbRedLabel: UILabel!
+  @IBOutlet weak var p3GreenLabel: UILabel!
+  @IBOutlet weak var rgbGreenLabel: UILabel!
+  @IBOutlet weak var p3BlueLabel: UILabel!
+  @IBOutlet weak var rgbBlueLabel: UILabel!
+  
+  var colorWheel: ColorWheelController!
+  let formatter = NumberFormatter()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    formatter.minimumFractionDigits = 2
+    formatter.maximumFractionDigits = 2
+    
     themeSlider()
+    configureColorPicker()
+    
+    selectGamut(traitCollection.displayGamut)
+    setColor(UIColor(named: "rwGreen")!)
   }
   
+  @IBAction func gamutSelectionChanged(_ sender: Any) {
+    setGamut(gamut)
+  }
+  
+  @IBAction func brightnessChanged(_ sender: Any) {
+    setBrightness(brightness)
+  }
+  
+  var gamut: Gamut {
+    guard let gamut = Gamut(rawValue: gamutSelector.selectedSegmentIndex) else {
+      return .displayP3
+    }
+    
+    return gamut
+  }
+  var brightness: CGFloat {
+    return CGFloat(1.0 - slider.value)
+  }
+}
+
+// MARK: Gamut Selector
+extension ViewController {
+  
+  func selectGamut(_ displayGamut: UIDisplayGamut) {
+    let gamut = Gamut.from(displayGamut: displayGamut)
+    gamutSelector.selectedSegmentIndex = gamut.rawValue
+    setGamut(gamut)
+  }
+  
+  func setGamut(_ gamut: Gamut) {
+    colorWheel.gamut = gamut
+  }
+}
+
+// MARK: brightness
+
+extension ViewController {
+    func setBrightness(_ value: CGFloat) {
+        didPick(color: colorWheel.color)
+    }
+}
+
+// MARK: Color Wheel Controller Delegate
+
+extension ViewController: ColorWheelControllerDelegate {
+  func didPick(color: UIColor) {
+    updateColor(with: color)
+  }
+  
+  func updateColor(with hueColor: UIColor) {
+    var hue: CGFloat = 0
+    hueColor.getHue(&hue, saturation: nil, brightness: nil, alpha: nil)
+    
+    let b = self.brightness
+    let hueBrightnessColor = UIColor(hue: hue, saturation: 1, brightness: b, alpha: 1)
+    var finalColor: UIColor
+    
+    switch gamut {
+    case .displayP3:
+      var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0
+      hueBrightnessColor.getRed(&red, green: &green, blue: &blue, alpha: nil)
+      finalColor = UIColor(displayP3Red: red, green: green, blue: blue, alpha: 1)
+    case .sRGB:
+      finalColor = hueBrightnessColor
+    }
+    
+    setColor(finalColor, updateBrightness: false)
+  }
+  
+  func setColor(_ color: UIColor, updateBrightness: Bool = true) {
+    swatchImageView.backgroundColor = color
+    
+    displayP3Values(for: color)
+    displayRGBValues(for: color)
+    
+    if updateBrightness {
+      var brightness: CGFloat = 0
+      if color.getHue(nil, saturation: nil, brightness: &brightness, alpha: nil) == false {
+        print("failed to get brightness")
+        return
+      }
+      
+      slider.value = Float(1 - brightness)
+    }
+  }
+  
+  func displayP3Values(for color: UIColor) {
+    
+    // UIColors are in sRGB color space and expressed in extended sRGB even if they were created
+    // with UIColor.init(displayP3Red:green:blue:alpha:)
+    
+    // Convert color to P3 color space
+    guard let colorSpaceP3 = CGColorSpace(name: CGColorSpace.displayP3),
+      let cgP3Color = color.cgColor.converted(to: colorSpaceP3, intent: .defaultIntent, options: nil),
+      let components = cgP3Color.components,
+      components.count >= 3 else {
+        p3RedLabel.text = nil
+        p3GreenLabel.text = nil
+        p3BlueLabel.text = nil
+        return
+    }
+    
+    p3RedLabel.text = String(format: "%.02f", components[0])
+    p3GreenLabel.text = String(format: "%.02f", components[1])
+    p3BlueLabel.text = String(format: "%.02f", components[2])
+  }
+  
+  func displayRGBValues(for color: UIColor) {
+    var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0
+    color.getRed(&red, green: &green, blue: &blue, alpha: nil)
+    
+    rgbRedLabel.text = String(format: "%.02f", red)
+    rgbGreenLabel.text = String(format: "%.02f", green)
+    rgbBlueLabel.text = String(format: "%.02f", blue)
+  }
+}
+
+// MARK: Appearance customization
+
+extension ViewController {
   private func themeSlider() {
     slider.setThumbImage(#imageLiteral(resourceName: "Reticule"), for: .normal)
     
+    buildSliderGradient()
+    
+    trackBackground.layer.cornerRadius = 5
+    trackBackground.layer.borderColor = UIColor(white: 0.5, alpha: 1).cgColor
+    trackBackground.layer.borderWidth = 1
+  }
+  
+  private func buildSliderGradient() {
     let gradient = CAGradientLayer()
+    
     gradient.colors = [UIColor.white.cgColor, UIColor.black.cgColor]
     gradient.locations = [0, 1]
     gradient.startPoint = CGPoint(x: 0, y: 0.5)
     gradient.endPoint = CGPoint(x: 1, y: 0.5)
     gradient.frame = gradientContainer.bounds
-    gradientContainer.layer.addSublayer(gradient)
-
-    trackBackground.layer.cornerRadius = 5
-    trackBackground.layer.borderColor = UIColor(white: 0.5, alpha: 1).cgColor
-    trackBackground.layer.borderWidth = 1
     
+    gradientContainer.layer.addSublayer(gradient)
+  }
+  
+  private func configureColorPicker() {
+    
+    let storyboard = UIStoryboard(name:"Main", bundle: nil)
+    let picker = storyboard.instantiateViewController(withIdentifier: "ColorWheelController") as! ColorWheelController
+    picker.delegate = self
+    addChildViewController(picker)
+    picker.view.frame = pickerContainer.bounds
+    pickerContainer.addSubview(picker.view)
+    picker.didMove(toParentViewController: self)
+    
+    colorWheel = picker
   }
 }
